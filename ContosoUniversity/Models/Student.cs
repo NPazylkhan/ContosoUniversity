22c199759ac18805a3023bde34bb934a6ac9220f
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-05-13 12:17:45 +0600

    adding inheritance to models and edit context model

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -3,30 +3,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student:Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name ="Last Name")]
-        [StringLength(50,MinimumLength = 2)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        public string LastName { get; set; }
-
-        [Required]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        [StringLength(50, MinimumLength = 2)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        public string FirstMidName { get; set; }
-
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName => LastName + ", " + FirstMidName;
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
